cc88c908b6ea1577071b815444a36fe6a1c9d4f3
Jakub kub1x Podlaha <j.podlaha@gmail.com>
2013-04-16 00:39:37 -0600

    added sandbox for every programmer to play with

--- trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/MainActivity.java
+++ trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/MainActivity.java
@@ -1,15 +1,39 @@
 package com.ttu_swri.goggles;
 
+import com.ttu_swri.goggles.sandbox.AlejandroSandboxActivity;
+import com.ttu_swri.goggles.sandbox.AotuoSandboxActivity;
+import com.ttu_swri.goggles.sandbox.JakubSandboxActivity;
+import com.ttu_swri.goggles.sandbox.SebastianSandboxActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	final String D_ALEJA = "Alejandro";
+	final String D_AOTUO = "Aotuo";
+	final String D_JAKUB = "Jakub";
+	final String D_SEBAS = "Sebastian";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button b_aleja = (Button) findViewById(R.id.but_aleja);
+		Button b_aotuo = (Button) findViewById(R.id.but_aotuo);
+		Button b_jakub = (Button) findViewById(R.id.but_jakub);
+		Button b_sebas = (Button) findViewById(R.id.but_sebas);
+
+		b_aleja.setOnClickListener(butChangeView);
+		b_aotuo.setOnClickListener(butChangeView);
+		b_jakub.setOnClickListener(butChangeView);
+		b_sebas.setOnClickListener(butChangeView);
 	}
 
 	@Override
@@ -18,5 +42,34 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+
+	private OnClickListener butChangeView = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.but_aleja:
+				startActivity(new Intent(getBaseContext(),
+						AlejandroSandboxActivity.class));
+				break;
+
+			case R.id.but_aotuo:
+				startActivity(new Intent(getBaseContext(),
+						AotuoSandboxActivity.class));
+				break;
+
+			case R.id.but_jakub:
+				startActivity(new Intent(getBaseContext(),
+						JakubSandboxActivity.class));
+				break;
+
+			case R.id.but_sebas:
+				startActivity(new Intent(getBaseContext(),
+						SebastianSandboxActivity.class));
+				break;
+			}
+		}
+	};
 
 }
