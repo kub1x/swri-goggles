48429d30b80790ca3bc84b0afc6b1ea36a78ec60
Jakub Podlaha <j.podlaha@gmail.com>
2013-04-17 00:44:43 -0600

    created simple example of how to use elements from data model

--- trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/sandbox/JakubSandboxActivity.java
+++ trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/sandbox/JakubSandboxActivity.java
@@ -1,23 +1,54 @@
 package com.ttu_swri.goggles.sandbox;
 
+import com.ttu_swri.datamodel.Element;
+import com.ttu_swri.datamodel.ElementMate;
+import com.ttu_swri.datamodel.ElementMessage;
+import com.ttu_swri.datamodel.ElementPoi;
+import com.ttu_swri.goggles.DataManager;
 import com.ttu_swri.goggles.R;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.app.Activity;
-import android.view.Menu;
 
 public class JakubSandboxActivity extends Activity {
+
+	// TESTING data model, DataManager and custom Visualizer
+	DataManager dm = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jakub_sandbox);
+
+		createData();
+		showData();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.jakub_sandbox, menu);
-		return true;
+	void createData() {
+		this.dm = new DataManager();
+		{
+			this.dm.update(new ElementMate());
+			this.dm.update(new ElementMate("Anand",
+					"The one with sorted closet. "));
+			this.dm.update(new ElementPoi());
+			this.dm.update(new ElementPoi("Woody",
+					"horse statue by Murray hall", null));
+			this.dm.update(new ElementMessage());
+			this.dm.update(new ElementMessage("argh!", "they killed me!!"));
+			this.dm.update(new ElementMate("Austin", "The Fruit King. "));
+			this.dm.update(new ElementMate("Vlad", "\"I Love Rock'n'Roll!\""));
+		}
+	}
+
+	void showData() {
+		LinearLayout lv = (LinearLayout) findViewById(R.id.j_sand_lin_lay);
+		lv.removeAllViews();
+		for (Element el : this.dm.getElements()) {
+			Button but = new Button(getBaseContext());
+			but.setText(el.Id);
+			lv.addView(but);
+		}
 	}
 
 }
