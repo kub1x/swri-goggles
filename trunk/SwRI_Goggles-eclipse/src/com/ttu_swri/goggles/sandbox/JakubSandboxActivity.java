7fbddd242a75bd430ec91f9fce4d44438a0dcbbc
Jakub Podlaha <j.podlaha@gmail.com>
2013-05-08 06:55:11 -0600

    created navigation in JakubSandboxActivity.java

--- trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/sandbox/JakubSandboxActivity.java
+++ trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/goggles/sandbox/JakubSandboxActivity.java
@@ -1,241 +1,219 @@
 package com.ttu_swri.goggles.sandbox;
 
-import java.text.DecimalFormat;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.app.TabActivity;
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TabHost;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.ttu_swri.datamodel.Element;
-import com.ttu_swri.datamodel.ElementMate;
-import com.ttu_swri.datamodel.ElementMessage;
-import com.ttu_swri.datamodel.ElementPoi;
-import com.ttu_swri.datamodel.IVisualizer;
-import com.ttu_swri.goggles.DataManager;
 import com.ttu_swri.goggles.R;
-import com.ttu_swri.goggles.network.NetworkSyncService;
 
-public class JakubSandboxActivity extends TabActivity {
-	@SuppressWarnings("unused")
+public class JakubSandboxActivity extends Activity {
 	private static final String TAG = JakubSandboxActivity.class
 			.getSimpleName();
 
-	private ButtonVisualizer bv;
-    private TabHost tabHost;
+	// TODO COPY THIS
+	private SensorManager mSensorManager;
+	private ArrowView mArrowView; // see ArrowView class below
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tabview);
-		
-        tabHost = getTabHost();
-        tabHost.addTab(tabHost.newTabSpec("Friendlies").setIndicator("Friendlies").setContent(R.id.layout_tab_friends));
-        tabHost.addTab(tabHost.newTabSpec("Notifications").setIndicator("Notifications").setContent(R.id.layout_tab_messages));
-        tabHost.addTab(tabHost.newTabSpec("Markers").setIndicator("Markers").setContent(R.id.layout_tab_pois));		
+		setContentView(R.layout.activity_jakub_sandbox);
 
-//		// Visualizer
-//		bv = new ButtonVisualizer(
-//				(LinearLayout) findViewById(R.id.j_sand_datalist));
-//		DataManager dm = DataManager.getInstance();
-//		dm.register(bv);
-
-		// Example data for testing POST
-		// Element element = new ElementMessage("test",
-		// "testing message for engage");
-		// dm.update(element);
+		// TODO COPY THIS
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mArrowView = new ArrowView(getBaseContext()); // see ArrowView class
+														// below
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		start();
+
+		// TODO COPY THIS - INCLUDE IN onResume()
+		mSensorManager.registerListener(mArrowView,
+				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+		mSensorManager.registerListener(mArrowView,
+				mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		stop();
+
+		// TODO COPY THIS - INCLUDE IN onPause
+		mSensorManager.unregisterListener(mArrowView);
 	}
 
-	// Handle network sync ----------------------------------------------------
-	
-	Timer timer;
-	TimerTask task;
+	// TODO IMPLEMENT THIS!!
+	// This is method that shows the navigation info.
+	// this is what should change the direction of arrow.
+	void showText(String text) {
+		((TextView) findViewById(R.id.j_sand_navigation)).setText(text);
+	}
 
-	void start() {
-		int delay = 0; // now
-		int period = 5000; // 5 sec
-		timer = new Timer();
-		task = new TimerTask() {
+	/**
+	 * TODO Include following class in class of that Arrow picture.
+	 * 
+	 * Following class contains all the code necessary to navigate user to some
+	 * position.
+	 * 
+	 * Target is hardcoded on campus on Texas Tech.
+	 * 
+	 * 
+	 * @author kub1x
+	 * 
+	 */
+	class ArrowView extends View implements SensorEventListener {
 
-			@Override
-			public void run() {
-				startService(new Intent(NetworkSyncService.NETWORK_SYNC_SERVICE));
+		private Location origin; // My Position
+		private Location target; // Final Position
+
+		double azimuth = 0; // here I store current azimuth
+
+		public ArrowView(Context context) {
+			super(context);
+
+			// HardCoded Target - Horse statue next to Murray Hall
+			target = new Location(LocationManager.PASSIVE_PROVIDER);
+			target.setLatitude(33.586661);
+			target.setLongitude(-101.879246);
+
+			// Current position
+			LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0,
+					new LocationListener() {
+
+						@Override
+						public void onStatusChanged(String provider,
+								int status, Bundle extras) {
+						}
+
+						@Override
+						public void onProviderEnabled(String provider) {
+						}
+
+						@Override
+						public void onProviderDisabled(String provider) {
+						}
+
+						@Override
+						public void onLocationChanged(Location location) {
+							updateLocation(location);
+						}
+					});
+		}
+
+		void updateLocation(Location location) {
+			this.origin = location;
+			navigate();
+		}
+
+		void navigate() {
+			if (origin == null || target == null)
+				return;
+			double bearing = origin.bearingTo(target);
+			double direction = normalize_angle(azimuth - bearing);
+
+			double distance = origin.distanceTo(target);
+
+			String text = "Azimuth: " + azimuth + "\n" + "Bearing: " + bearing
+					+ "\n" + "Dir: " + direction + "\n" + "Dist: " + distance;
+
+			if (distance < 10) {
+				// stop
+				text += "\n" + "stop";
+			} else {
+
+				// Forward direction is +/- "trashhold" degrees
+				double trashhold = 20;
+
+				if (direction < trashhold && direction > -trashhold) {
+					text += "\n" + "forward";
+					// forward
+				}
+
+				if (direction >= trashhold && direction < 180 - trashhold) {
+					text += "\n" + "left";
+					// left
+				}
+
+				if (direction <= -trashhold && direction > -180 + trashhold) {
+					text += "\n" + "right";
+					// right
+				}
+
+				if (direction <= -180 + trashhold
+						|| direction >= 180 - trashhold) {
+					text += "\n" + "behind";
+					// behind
+				}
 			}
-		};
-		timer.scheduleAtFixedRate(task, delay, period);
-	}
-
-	void stop() {
-		timer.cancel();
-	}
-
-	// ==============================================================================
-	// ==============================================================================
-	// ==============================================================================
-
-	// private void startGps() {
-	// // Start GPS service
-	// // startService(new Intent(this, MyPositionUpdateService.class));
-	//
-	// LocationManager lm = (LocationManager)
-	// getSystemService(Context.LOCATION_SERVICE);
-	// lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0,
-	// new LocationListener() {
-	// DecimalFormat df = new DecimalFormat("#.000000");
-	//
-	// @Override
-	// public void onStatusChanged(String provider, int status,
-	// Bundle extras) {
-	// // TODO Auto-generated method stub
-	// }
-	//
-	// @Override
-	// public void onProviderEnabled(String provider) {
-	// // TODO Auto-generated method stub
-	// }
-	//
-	// @Override
-	// public void onProviderDisabled(String provider) {
-	// // TODO Auto-generated method stub
-	// }
-	//
-	// @Override
-	// public void onLocationChanged(Location location) {
-	// TextView tv = (TextView) findViewById(R.id.j_sand_location);
-	// String text = "Loc: "
-	// + df.format(location.getLatitude()) + ", "
-	// + df.format(location.getLongitude());
-	//
-	// tv.setText(text);
-	// }
-	// });
-	//
-	// }
-
-	private class ButtonVisualizer implements IVisualizer {
-
-		DataManager dm;
-		LinearLayout ll;
-
-		public ButtonVisualizer(LinearLayout ll) {
-			this.dm = DataManager.getInstance();
-			this.ll = ll;
+			showText(text);
 		}
 
 		@Override
-		public void update() {
-			ll.removeAllViews();
-			for (Element el : this.dm.getElements()) {
-				MyButton but = new MyButton(getBaseContext(), el);
-				ll.addView(but);
-			}
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+			// DO NOTHING
 		}
 
-		private class MyButton extends Button {
+		float[] mGravity;
+		float[] mGeomagnetic;
 
-			DecimalFormat df = new DecimalFormat("#.0000");
-			// private Element element;
-			String location = "";
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+				mGravity = event.values.clone();
 
-			private Paint m_paint = new Paint();
+			if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+				mGeomagnetic = event.values.clone();
 
-			// private int m_color = 0XFF92C84D; // LIKE AN OLIVE GREEN..
+			if (mGravity != null && mGeomagnetic != null) {
+				float R[] = new float[9];
+				float I[] = new float[9];
+				float rR[] = new float[9];
 
-			public MyButton(Context context, Element element) {
-				super(context);
+				boolean success = SensorManager.getRotationMatrix(R, I,
+						mGravity, mGeomagnetic);
 
-				// this.element = element;
-
-				Location loc;
-				switch (element.getType()) {
-				case T_MATE:
-					setText(((ElementMate) element).getName());
-					loc = ((ElementMate) element).getLocation();
-					double lat = loc.getLatitude();
-					double lon = loc.getLongitude();
-					location = "Loc: " + df.format(lat) + ", " + df.format(lon);
-					break;
-				case T_POI:
-					setText(((ElementPoi) element).getName());
-					loc = ((ElementPoi) element).getLocation();
-					location = df.format(loc.getLatitude()) + "\n"
-							+ df.format(loc.getLongitude());
-					break;
-				case T_MESSAGE:
-					setText(((ElementMessage) element).getTopic());
-					break;
-				default:
+				if (success) {
+					SensorManager.remapCoordinateSystem(R,
+							SensorManager.AXIS_X, SensorManager.AXIS_Z, rR);
+					float orientation[] = new float[3];
+					SensorManager.getOrientation(rR, orientation);
+					azimuth = orientation[0]; // azimuth, pitch and roll
 				}
-
-				this.setOnClickListener((OnClickListener) new MyOnClickListener(
-						element));
 			}
 
-			@Override
-			protected void onDraw(Canvas canvas) {
-				super.onDraw(canvas);
-				m_paint.setColor(Color.DKGRAY);
-				canvas.drawText(location, 5, 15, m_paint);
-			}
+			// Corelation for goggles
+			// and fix btwn 180 and -180 after corelation
+			final double corelation = -70;
+			azimuth = normalize_angle(Math.toDegrees(azimuth) + corelation);
 
-			class MyOnClickListener implements OnClickListener {
-				Element element;
+			navigate(); // i.e. invalidate(); updates according to new
+						// conditions
+		}
 
-				public MyOnClickListener(Element element) {
-					this.element = element;
-				}
-
-				@Override
-				public void onClick(View arg0) {
-					switch (element.getType()) {
-					case T_MATE:
-						toast(((ElementMate) element).getDescription());
-						break;
-					case T_POI:
-						toast(((ElementPoi) element).getDescription());
-						break;
-					case T_MESSAGE:
-						toast(((ElementMessage) element).getText());
-						break;
-					default:
-						toast("unknown element: " + element.getId());
-					}
-				}
-
-				void toast(String text) {
-					Toast prName = Toast.makeText(getBaseContext(), text,
-							Toast.LENGTH_LONG);
-					prName.setGravity(Gravity.AXIS_SPECIFIED, 0, 0);
-					prName.show();
-				}
-
-			} // class MyOnClickListener
-
-		} // class MyButton
-
-	} // class ButtonVisualizer
+		/**
+		 * get angle back between -180 and 180 after addition/substraction
+		 * 
+		 * @param angle
+		 * @return
+		 */
+		double normalize_angle(double angle) {
+			return ((angle + 360 + 180) % 360) - 180;
+		}
+	}
 
 }
