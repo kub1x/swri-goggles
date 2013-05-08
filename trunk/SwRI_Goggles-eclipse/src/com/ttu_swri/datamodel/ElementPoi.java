f482117e132f9ea38de2451906de76141b8e4bd6
Alex R <xandro.rocha@gmail.com>
2013-05-08 04:54:35 -0500

    Missed one

--- trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/datamodel/ElementPoi.java
+++ trunk/SwRI_Goggles-eclipse/src/com/ttu_swri/datamodel/ElementPoi.java
@@ -64,7 +64,7 @@ public class ElementPoi extends Element {
 
 		this.name = name;
 		this.description = description;
-		this.location = location;
+		this.setLocation(location);
 		this.checkpointNumber = checkpointNumber;
 		this.expires = expires;
 	}
